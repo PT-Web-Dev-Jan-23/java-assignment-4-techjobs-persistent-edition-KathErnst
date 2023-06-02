3a0b9593465ec600f5f27fe05849256779809c26
katherineernsting <katherine.e.kennison@gmail.com>
2023-06-01 23:46:05 -0500

    Persisting employers and skills

--- queries.sql
+++ queries.sql
@@ -2,6 +2,9 @@
 -- id (int PK), employer (varchar(255)), name (varchar(255)), skills (varchar(255))
 
 -- Part 2: Test it with SQL
+    SELECT name
+    FROM employer
+    WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
 
