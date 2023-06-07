cf57b3ac5497c9efdd152de95e64c1fd02dde5ce
katherineernsting <katherine.e.kennison@gmail.com>
2023-06-06 23:38:58 -0500

    Completing Task 4, everything appears to be working but one test is not passing

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -2,6 +2,7 @@ package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.Skill;
 import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
 import org.launchcode.techjobs.persistent.models.data.JobRepository;
 import org.launchcode.techjobs.persistent.models.data.SkillRepository;
@@ -60,6 +61,8 @@ public class HomeController {
             } else {
                 Employer employer = result.get();
                 newJob.setEmployer(employer);
+                List<Skill> skillObjs = (List<Skill>) skillRepository.findAllById(skills);
+                newJob.setSkills(skillObjs);
                 jobRepository.save(newJob);
             }
         }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,11 +1,17 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
 
     private String description;
+
+    @ManyToMany (mappedBy="skills")
+    private final List<Job> jobs = new ArrayList<>();
 
     public Skill () {}
 
@@ -14,6 +20,13 @@ public class Skill extends AbstractEntity {
     }
 
     public void setDescription(String description) {
-        this.description = description;
+        this.description = description;}
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void addJobs(Job job) {
+        this.jobs.add(job);
     }
 }
